2cd3d2a0db1159a465292cad25d732e64d498bdd
Your Name <mohammadabdoli9577@gmail.com>
2025-12-09 14:19:03 +0330

    sbudu

--- app/(players)/videos/components/VideoCard.tsx
+++ app/(players)/videos/components/VideoCard.tsx
@@ -21,37 +21,39 @@ export function VideoCard({
                           }: VideoCardProps) {
 
     return (
-        <div className="flex items-center gap-4 bg-[#1a1a1a] border border-neutral-700 px-4 py-2 rounded-md shadow w-fit">
+        <div className="flex items-center gap-16 bg-[#1a1a1a] border border-neutral-700 px-4 py-2 rounded-md shadow w-fit">
 
             <div className="flex flex-col">
                 <span className="text-white font-semibold text-sm">{title}</span>
                 <span className="text-white/50 text-xs">{code}</span>
             </div>
-            {/* Remove Button */}
-            <Button
-                variant="ghost"
-                size="icon"
-                onClick={onRemove}
-                className="text-white/70 hover:text-red-400 hover:bg-transparent"
-            >
-                <Minus size={18} />
-            </Button>
-            {/* Play Button */}
-            <Button
-                variant="ghost"
-                size="icon"
-                onClick={onPlay}
-                className="text-white/80 hover:text-white hover:bg-transparent"
-            >
-                <Video size={18} />
-            </Button>
-            {/* Normal HTML Checkbox */}
-            <input
-                type="checkbox"
-                checked={checked}
-                onChange={onCheck}
-                className="w-4 h-4 cursor-pointer accent-white"
-            />
+            <div className='flex items-center justify-center'>
+                {/* Remove Button */}
+                <Button
+                    variant="ghost"
+                    size="icon"
+                    onClick={onRemove}
+                    className="text-white/70 hover:text-red-400 hover:bg-transparent"
+                >
+                    <Minus size={18} />
+                </Button>
+                {/* Play Button */}
+                <Button
+                    variant="ghost"
+                    size="icon"
+                    onClick={onPlay}
+                    className="text-white/80 hover:text-white hover:bg-transparent"
+                >
+                    <Video size={18} />
+                </Button>
+                {/* Normal HTML Checkbox */}
+                <input
+                    type="checkbox"
+                    checked={checked}
+                    onChange={onCheck}
+                    className="w-4 h-4 cursor-pointer accent-white"
+                />
+            </div>
         </div>
     );
 }
