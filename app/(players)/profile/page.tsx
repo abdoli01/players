cc76f58594d732f9c0855f4185db4c45102712c0
Your Name <mohammadabdoli9577@gmail.com>
2025-12-13 19:45:13 +0330

    vcgvtv

--- app/(players)/profile/page.tsx
+++ app/(players)/profile/page.tsx
@@ -1,11 +1,10 @@
 "use client"
 import React from 'react'
-import ReactPlayer from 'react-player'
 
 const Page = () => {
     return (
         <div>
-            <ReactPlayer src='https://www.youtube.com/watch?v=LXb3EKWsInQ' />
+            Profile
         </div>
     );
 };
