dd4063fe06b9dfe29a8b304c39882f058f4988af
Your Name <mohammadabdoli9577@gmail.com>
2025-12-14 14:54:22 +0330

    tftfd

--- app/(players)/videos/page.tsx
+++ app/(players)/videos/page.tsx
@@ -4,12 +4,20 @@ import { VideoCard } from "@/app/(players)/videos/components/VideoCard";
 import CustomVideoPlayer from "@/app/(players)/videos/components/CustomVideoPlayer";
 import videos from "@/data/videos.json";
 import {Monitor} from "lucide-react";
+import {Button} from "@/components/ui/button";
+import { List, Grid } from 'lucide-react';
 
 
 const Page = () => {
     const [index, setIndex] = useState(-1);
     const [monitorActive, setMonitorActive] = useState(false);
     const [forcePlay, setForcePlay] = useState(0);
+
+    const [showFirstList, setShowFirstList] = useState(true);
+    const [selectedIndex, setSelectedIndex] = useState(-1); // ذخیره آیتم انتخاب شده
+
+    const items1=['ATTACKING','PASSING','DULES','DEFENSIVE','ALL','ACTIONS']
+    const items2=['RGRGRG','PASSIHHTHTHTHHTNG','HTH','RHRHRTH','HRHRHRHRHRTHTR','HT']
 
     const currentVideo = videos[index];
     console.log('currentVideo', currentVideo);
@@ -28,11 +36,34 @@ const Page = () => {
         // setMonitorActive(false); // ← وقتی روی Play کلیک شد، مانیتور خاموش می‌شه
         setForcePlay(prev => prev + 1); // ← ریست و پخش دوباره
     };
+    const toggleList = () => {
+        setShowFirstList(!showFirstList);
+        setSelectedIndex(-1); // هر بار لیست عوض شد انتخاب پاک شود
+    };
+    const currentItems = showFirstList ? items1 : items2;
 
     return (
         <div className="py-4">
+            {/*items*/}
+            <div className="flex items-center mb-4 gap-2">
+                <Button onClick={toggleList} className="flex items-center gap-2">
+                    {showFirstList ? <List size={20} /> : <Grid size={20} />}
+                </Button>
+                <div className="flex flex-wrap gap-2">
+                    {currentItems.map((item, index) => (
+                        <Button
+                            key={index}
+                            onClick={() => setSelectedIndex(index)}
+                            className={`${
+                                selectedIndex === index ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black'
+                            }`}
+                        >
+                            {item}
+                        </Button>
+                    ))}
+                </div>
+            </div>
             <div className="grid grid-cols-12 gap-4">
-
                 {/* لیست ویدیوها */}
                 <div className="col-span-12 lg:col-span-4 flex flex-col gap-2 h-[400px] overflow-y-scroll order-2 lg:order-1">
                     {videos.map((video, i) => (
