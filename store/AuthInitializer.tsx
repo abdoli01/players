5e852146e399fc5552ebfe4b98fa2d513e34d92a
Your Name <mohammadabdoli9577@gmail.com>
2025-12-21 13:31:32 +0330

    vggft

--- store/AuthInitializer.tsx
+++ store/AuthInitializer.tsx
@@ -2,21 +2,18 @@
 "use client";
 
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 import { useAppDispatch } from "./hooks";
 import { setUser, clearUser } from "./slices/userSlice";
 import { profileService } from "@/services/auth";
 
 export default function AuthInitializer() {
     const dispatch = useAppDispatch();
-    const router = useRouter();
 
     useEffect(() => {
         const token = localStorage.getItem("access_token");
 
         if (!token) {
             dispatch(clearUser());
-            router.replace("/login"); // ✅ مهم
             return;
         }
 
@@ -28,9 +25,8 @@ export default function AuthInitializer() {
             .catch(() => {
                 localStorage.removeItem("access_token");
                 dispatch(clearUser());
-                router.replace("/login");
             });
-    }, [dispatch, router]);
+    }, [dispatch]);
 
     return null;
 }
