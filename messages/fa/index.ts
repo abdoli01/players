a99f7523cca0552c58f4cd0a53b3f1cd562f8ba1
Your Name <mohammadabdoli9577@gmail.com>
2025-12-27 13:18:57 +0330

    eged

--- messages/fa/index.ts
+++ messages/fa/index.ts
@@ -1,8 +1,10 @@
 import home from './home.json';
 import navbar from './navbar.json';
 import sideBar from './app-sidebar.json';
+import auth from './auth.json';
 export default {
     ...home,
     ...sideBar,
-    ...navbar
+    ...navbar,
+    ...auth
 };
