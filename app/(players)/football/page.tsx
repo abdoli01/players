21355c1250e943a91b5bd19e29385c87555b2bfa
Your Name <mohammadabdoli9577@gmail.com>
2025-12-28 17:06:11 +0330

    es

--- app/(players)/football/page.tsx
+++ app/(players)/football/page.tsx
@@ -8,15 +8,15 @@ type Player = {
 }
 
 const startingXI: Player[] = [
-    { id: 1, number: 1, top: "50%", left: "7%" },
+    { id: 1, number: 1, top: "50%", left: "6%" },
 
     { id: 2, number: 4, top: "18%", left: "30%" },
     { id: 3, number: 5, top: "32%", left: "30%" },
     { id: 4, number: 6, top: "55%", left: "30%" },
     { id: 5, number: 3, top: "72%", left: "30%" },
 
-    { id: 6, number: 7, top: "45%", left: "50%" },
-    { id: 7, number: 10, top: "55%", left: "50%" },
+    { id: 6, number: 7, top: "46%", left: "50%" },
+    { id: 7, number: 10, top: "56%", left: "50%" },
 
     { id: 8, number: 70, top: "18%", left: "70%" },
     { id: 9, number: 23, top: "32%", left: "82%" },
@@ -29,42 +29,41 @@ const substitutes = [77, 8, 2, 11, 69, 93]
 export default function FootballPitch() {
     return (
         <div className="bg-black p-6 rounded-xl w-fit">
-
             {/* FIELD */}
-            <div className="relative w-[820px] aspect-[2.25/1] border-2 border-gray-500">
+            <div className="relative w-[820px] aspect-[2.25/1] border-2 border-gray-400">
 
-                {/* Inner Border */}
+                {/* INNER BORDER */}
                 <div className="absolute inset-4 border border-gray-500" />
 
-                {/* Half Line */}
+                {/* HALF LINE */}
                 <div className="absolute top-4 bottom-4 left-1/2 w-px bg-gray-500" />
 
-                {/* Center Circle */}
+                {/* CENTER CIRCLE */}
                 <div className="absolute top-1/2 left-1/2 w-28 h-28 border border-gray-500 rounded-full -translate-x-1/2 -translate-y-1/2" />
 
-                {/* ---------- LEFT SIDE ---------- */}
+                {/* ================= LEFT SIDE ================= */}
 
-                {/* Penalty Area */}
-                <div className="absolute top-[18%] left-4 w-40 h-[64%] border border-gray-500" />
+                {/* PENALTY AREA */}
+                <div className="absolute top-[18%] left-4 w-44 h-[64%] border border-gray-500" />
 
-                {/* Goal Area */}
-                <div className="absolute top-[38%] left-4 w-20 h-[24%] border border-gray-500" />
+                {/* GOAL AREA (بزرگ‌تر) */}
+                <div className="absolute top-[34%] left-4 w-24 h-[32%] border border-gray-500" />
 
-                {/* Penalty Arc (OUTSIDE only) */}
-                <div className="absolute top-1/2 left-[98px] w-28 h-28 border border-gray-500 rounded-full -translate-y-1/2 clip-left" />
+                {/* PENALTY ARC (OUTSIDE ONLY) */}
+                <div className="absolute top-1/2 left-[112px] w-28 h-28 border border-gray-500 rounded-full -translate-y-1/2 clip-left" />
 
-                {/* ---------- RIGHT SIDE ---------- */}
+                {/* ================= RIGHT SIDE ================= */}
 
-                {/* Penalty Area */}
-                <div className="absolute top-[18%] right-4 w-40 h-[64%] border border-gray-500" />
+                {/* PENALTY AREA */}
+                <div className="absolute top-[18%] right-4 w-44 h-[64%] border border-gray-500" />
 
-                {/* Goal Area */}
-                <div className="absolute top-[38%] right-4 w-20 h-[24%] border border-gray-500" />
+                {/* GOAL AREA */}
+                <div className="absolute top-[34%] right-4 w-24 h-[32%] border border-gray-500" />
 
-                {/* Penalty Arc (OUTSIDE only) */}
-                <div className="absolute top-1/2 right-[98px] w-28 h-28 border border-gray-500 rounded-full -translate-y-1/2 clip-right" />
+                {/* PENALTY ARC (OUTSIDE ONLY) */}
+                <div className="absolute top-1/2 right-[112px] w-28 h-28 border border-gray-500 rounded-full -translate-y-1/2 clip-right" />
 
-                {/* PLAYERS */}
+                {/* ================= PLAYERS ================= */}
                 {startingXI.map(player => (
                     <div
                         key={player.id}
@@ -92,7 +91,7 @@ export default function FootballPitch() {
                 ))}
             </div>
 
-            {/* CLIP HELPERS */}
+            {/* CLIP PATHS */}
             <style jsx>{`
         .clip-left {
           clip-path: inset(0 0 0 70%);
