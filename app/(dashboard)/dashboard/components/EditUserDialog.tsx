a5935fd383b067083ec1ce3bba477cdbd23c745c
Your Name <mohammadabdoli9577@gmail.com>
2025-12-31 14:53:16 +0330

    dwdw

--- app/(dashboard)/dashboard/components/EditUserDialog.tsx
+++ app/(dashboard)/dashboard/components/EditUserDialog.tsx
@@ -200,6 +200,11 @@ export function EditUserDialog({ user }: Props) {
                         </Button>
                     </DialogFooter>
                 </form>
+                <style>{`
+                          button.right-4 {
+                            ${isRtl ? 'right: auto; left: 1rem;' : 'left: auto; right: 1rem;'}
+                          }
+                    `}</style>
             </DialogContent>
         </Dialog>
     );
