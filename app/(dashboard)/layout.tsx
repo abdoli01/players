90cab8e8be6de25df9d35f404d6bc6d6687d250c
Your Name <mohammadabdoli9577@gmail.com>
2026-02-01 14:34:06 +0330

    add logo in mobile dashboard

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { DashboardSidebar } from "@/app/(dashboard)/dashboard/components/DashboardSidebar"
 import AdminGate from "@/components/AdminGate";
+import Link from "next/link";
+import {cn} from "@/lib/utils";
+import Image from "next/image";
 
 const LayoutDashboard = ({children}:{children:React.ReactNode}) => {
     return (
@@ -13,8 +16,25 @@ const LayoutDashboard = ({children}:{children:React.ReactNode}) => {
             } as React.CSSProperties
         }>
             <DashboardSidebar />
-            <main className={'p-4 w-full overflow-x-auto'}>
-                    <SidebarTrigger className='md:hidden' />
+            <main className={'px-4 w-full overflow-x-auto'}>
+                    <div className='md:hidden flex justify-between items-center'>
+                        <SidebarTrigger className='md:hidden' />
+                        <Link
+                            href='/'
+                            className={cn(
+                                "flex flex-col items-center justify-center gap-2 px-3 py-2 transition-colors h-[65px]",
+                            )}
+                        >
+                            <div className="relative w-6 h-8 md:w-12 md:h-14 block md:hidden">
+                                <Image
+                                    src="/images/logo-new.png"
+                                    alt="logo"
+                                    fill
+                                    className="object-contain"
+                                />
+                            </div>
+                        </Link>
+                    </div>
                 {children}
             </main>
         </SidebarProvider>
