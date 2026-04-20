f98a696a176fd3c3197589f6cae846e874ac007b
Your Name <mohammadabdoli9577@gmail.com>
2026-04-20 16:30:01 +0330

    vdvd

--- components/UserColorProvider.tsx
+++ components/UserColorProvider.tsx
@@ -3,9 +3,13 @@
 import * as React from "react";
 import { useGetMyColorPaletteQuery } from "@/services/api/usersApi";
 import { applyUserColors } from "@/utils/applyUserColors";
+import { useAppSelector } from "@/store/hooks";
+
 
 export function UserColorProvider() {
-    const { data } = useGetMyColorPaletteQuery();
+    const user = useAppSelector(s => s.user.user); // ✅ اضافه شد
+
+    const { data } = useGetMyColorPaletteQuery(undefined,{ skip: !user});
 
     React.useEffect(() => {
         if (!data) return;
